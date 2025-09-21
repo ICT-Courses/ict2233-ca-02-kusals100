87cf7bca55d6569c05cbf8435154075ab21f592b
kusals100 <ar112481@fhss.sjp.ac.lk>
2025-09-22 02:42:19 +0530

    feat: Implement basic application  layout and routing with Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,27 @@
 import {Routes , Route } from 'react-router-dom';
-import navigationbar from './components/navigationbar'
+import Navbar from './components/Navbar';
 import Home from './pages/Home';
 import About from './pages/About';
+import Portfolio from './pages/Portfolio';
+import Contact from './pages/Contact';
 
 
 
 function App() {
 
   return (
-    <div>
-      <h1>My portfolio web site</h1>
+    <div className='bg-[#0a192f] text-gray-300 min-h-screen'>
+      
+  <Navbar/>
+      <main>
+        <Routes>
+          <Route path='/' element={<Home/>}></Route>
+          <Route path='/about' element={<About/>}/>
+          <Route path='/portfolio' element={<Portfolio/>}/>
+          <Route path='/Contact' element={<Contact/>}/>
+        </Routes>
+      </main>
+      
     </div>
   )
 }
