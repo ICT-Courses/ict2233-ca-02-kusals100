fa5a9524f910b40a71da6390fa306c138d687b77
kusals100 <ar112481@fhss.sjp.ac.lk>
2025-09-24 01:57:43 +0530

    style: Ensure all sections are full-screen for a proper single-page layout

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,7 +2,12 @@ import React from 'react'
 
 function About() {
   return (
-    <div name="about">About</div>
+    <div name="about" className='w-full h-screen bg-[#0a192f] text-gray-300'>About
+      <div className ='flex flex-col justify-center items-center w-full h-full'>
+        <p className ='text-4xl font-bold'> This is the About Section </p>
+      </div>
+    </div>
+   
   )
 }
 
--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 function Portfolio() {
   return (
-    <div name = "portfolio">Portfolio</div>
+    <div name ='portfolio' className='w-full h-screen bg-[#0a192f] text-gray-300'>
+      <div className ='flex flex-col justify-center items-center w-full h-full'>
+        <p className ='text-4xl font-bold'>This is the Portfolio Section</p>
+      </div>
+    </div>
   )
 }
 
