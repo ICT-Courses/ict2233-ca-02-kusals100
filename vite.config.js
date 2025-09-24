2f0b0d219f11f722090ecd940737c6a61d117cdc
kusals100 <ar112481@fhss.sjp.ac.lk>
2025-09-24 23:21:55 +0530

    feat:configure pachage.json and vite.config.js to deployment

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/ict2233-ca-02-kusals100/",
   plugins: [react(),
   tailwindcss(),],
 })
