d11750915dbb40bcbedfd5e7d42375b19b8500fb
kusals100 <ar112481@fhss.sjp.ac.lk>
2025-09-24 17:04:39 +0530

    feat(portfolio): build static layout for the portfolio section

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,13 +1,49 @@
-import React from 'react'
+import React from 'react';
 
-function Portfolio() {
-  return (
-    <div name ='portfolio' className='w-full h-screen bg-[#0a192f] text-gray-300'>
-      <div className ='flex flex-col justify-center items-center w-full h-full'>
-        <p className ='text-4xl font-bold'>This is the Portfolio Section</p>
-      </div>
-    </div>
-  )
-}
+    function Portfolio() {
 
-export default Portfolio
+      return (
+        <div name='portfolio' className='w-full md:h-screen text-gray-300 bg-[#0a192f]'>
+          <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
+            <div className ='pb-8'>
+              <p className='inline text-4xl font-bold text-gray-300 border-b-4 border-pink-600'>
+                Portfolio
+              </p>
+              <p className='py-6'>// Checking out some of my recent work</p>
+            </div>
+
+
+            {/* Grid Container for Cards */}
+            <div className='grid gap-4 sm:grid-cols-2 md:grid-cols-3'>
+              
+
+              {/* Placeholder Card 1 */}
+              <div className ='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>
+                <div className ="p-4">
+                  <span className ='text-2xl font-bold tracking-wider text-white'>
+                    Project Name
+                  </span>
+                  <div className ='pt-8 text-center'>
+                    <a href="/">
+                      <button className ='px-4 py-3 m-2 text-lg font-bold text-center text-gray-700 bg-white rounded-lg'>
+                        Demo
+                      </button>
+                    </a>
+                    <a href="/">
+                      <button className ='px-4 py-3 m-2 text-lg font-bold text-center text-gray-700 bg-white rounded-lg'>
+                        Code
+                      </button>
+
+                    </a>
+                  </div>
+                </div>
+              </div>
+              {/* End of Placeholder Card */}
+
+            </div>
+          </div>
+        </div>
+      );
+    }
+
+export default Portfolio;
